df44cb9ce2f6e2a59d225e7d9582fbdd142cf044
den.ysp <33653989+mettugi@users.noreply.github.com>
2021-12-18 23:06:25 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,11 @@
+describe('Login', () => {
+  it('Login Valid"', () => {
+    cy.visit('https://www.saucedemo.com/')
+
+    //cy.get('.user-name')
+      //.type(standard_user)
+      //.get('password')
+      //.type('secret_sauce')
+      //.click('login-button)
+  })
+})
