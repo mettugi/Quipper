a41e64ddb1f9aaba14919325fb62615e28987c47
den.ysp <33653989+mettugi@users.noreply.github.com>
2021-12-22 13:47:46 +0700

    Update add_to_cart_spec.js

--- add_to_cart_spec.js
+++ add_to_cart_spec.js
@@ -1,14 +1,60 @@
-describe('Add to cart', () => {
-    it('Add to Cart Product"', () => {
-      cy.visit('www.saucedemo.com')
-  
-      cy.get('[data-test="username"]')
-        .type('Standard_user')
-        .get('[data-test="password"]')
-        .type('secret_sauce')
-        .get('[data-test="login-button"]')
-        .click()
-        .get('[data-test="add-to-cart-sauce-labs-backpack"]')
-        .click()
+const Login = require('login.js')
+const addToCart = require('add_to_cart.js')
+const username = "standard_user"
+const password = "secret_sauce"
+
+describe('Add To Cart', () => {
+    it('Add Backpack', () => {
+      cy.visit('https://www.saucedemo.com/')
+      Login.inputUsername(username)
+      Login.inputPassword(password)
+      Login.clickLoginButton() 
+      addToCart.addToCartBackPack()
+    }
+  },
+describe('Add To Cart', () => {
+    it('Add BikeLight', () => {
+      cy.visit('https://www.saucedemo.com/')
+      Login.inputUsername(username)
+      Login.inputPassword(password)
+      Login.clickLoginButton() 
+      addToCart.addToCartBikeLight()
+    }
+  },
+describe('Add To Cart', () => {
+    it('Add BoltTshirt', () => {
+      cy.visit('https://www.saucedemo.com/')
+      Login.inputUsername(username)
+      Login.inputPassword(password)
+      Login.clickLoginButton() 
+      addToCart.addToCartBoltTshirt()
+    }
+  },
+describe('Add To Cart', () => {
+    it('Add Fleece Jacket', () => {
+      cy.visit('https://www.saucedemo.com/')
+      Login.inputUsername(username)
+      Login.inputPassword(password)
+      Login.clickLoginButton() 
+      addToCart.addToCartFleeceJacket()
+    }
+  },
+describe('Add To Cart', () => {
+    it('Add oneSie', () => {
+      cy.visit('https://www.saucedemo.com/')
+      Login.inputUsername(username)
+      Login.inputPassword(password)
+      Login.clickLoginButton() 
+      addToCart.addToCartOneSie()
+    }
+  },
+describe('Add To Cart', () => {
+    it('Add Tshirt Red', () => {
+      cy.visit('https://www.saucedemo.com/')
+      Login.inputUsername(username)
+      Login.inputPassword(password)
+      Login.clickLoginButton() 
+      addToCart.addToCartTShirtRed()
     }
   }
+       }
