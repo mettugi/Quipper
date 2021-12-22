1a7a1759836c0c4b124e73be740589cce3f14e01
den.ysp <33653989+mettugi@users.noreply.github.com>
2021-12-22 13:40:05 +0700

    Update login.js

--- login.js
+++ login.js
@@ -3,27 +3,22 @@ module.exports={
   this.inputLoginInformation(username,password)
   this.clickLoginButton()
 },
-
 inputLoginInformation(username,password){
 this.inputUsername(username)
 this.inputPassword(password)
 },
-
 clickLoginButton(){
 cy.get('[data-test="login-button"]')
   .click()
 },
-
 inputUsername(username){
   cy.get('[data-test="username"]')
     .type(username)
     .should('have.value', username)
 },
-
 inputPassword(password){
   cy.get('[data-test="password"]')
     .type(password)
     .should('have.value', password)
 },
-
 }
