71f5d1259373384e38626adf06ecbc5ee146ad2d
den.ysp <33653989+mettugi@users.noreply.github.com>
2021-12-22 13:38:25 +0700

    Update logout.js

--- logout.js
+++ logout.js
@@ -1,17 +1,15 @@
-describe('Logout', () => {
-    it('Logout', () => {
-      cy.visit('www.saucedemo.com')
-  
-      cy.get('[data-test="username"]')
-        .type('Standard_user')
-        .get('[data-test="password"]')
-        .type('secret_sauce')
-        .get('[data-test="login-button"]')
-        .click()
-        .get('[id="react-burger-menu-btn"]')
-        .click()
-        .get('[id="logout_sidebar_link"]')
-        .click()
+const Login = require('login.js')
+const burgerMenu = require('burger_menu.js')
+const username = "standard_user"
+const password = "secret_sauce"
 
+describe('Login', () => {
+    it('Login Valid"', () => {
+      cy.visit('https://www.saucedemo.com/')
+      Login.inputUsername(username)
+      Login.inputPassword(password)
+      Login.clickLoginButton() 
+      burgerMenu.clickBurgerMenu()
+      burgerMenu.clickLogout()
     })
   })
