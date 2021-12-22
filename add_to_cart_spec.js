5aadd6927a8b895edf16cdfebd4ce677f1581d8c
den.ysp <33653989+mettugi@users.noreply.github.com>
2021-12-22 13:17:49 +0700

    Add files via upload

---
+++ add_to_cart_spec.js
@@ -0,0 +1,14 @@
+describe('Add to cart', () => {
+    it('Add to Cart Product"', () => {
+      cy.visit('www.saucedemo.com')
+  
+      cy.get('[data-test="username"]')
+        .type('Standard_user')
+        .get('[data-test="password"]')
+        .type('secret_sauce')
+        .get('[data-test="login-button"]')
+        .click()
+        .get('[data-test="add-to-cart-sauce-labs-backpack"]')
+        .click()
+    })
+  })
