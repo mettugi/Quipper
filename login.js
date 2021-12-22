9084d23e95d2b4294c58efd35115b93e72f4dcfa
den.ysp <33653989+mettugi@users.noreply.github.com>
2021-12-22 13:16:22 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,12 +1,29 @@
-describe('Login', () => {
-  it('Login Valid"', () => {
-    cy.visit('www.saucedemo.com')
+module.exports={
+  loginValid(username,password){
+  this.inputLoginInformation(username,password)
+  this.clickLoginButton()
+},
 
-    cy.get('[data-test="username"]')
-      .type('standard_user')
-      .get('[data-test="password"]')
-      .type('secret_sauce')
-      .get('[data-test="login-button"]')
-      .click()
-  })
-})
+inputLoginInformation(username,password){
+this.inputUsername(username)
+this.inputPassword(password)
+},
+
+clickLoginButton(){
+cy.get('[data-test="login-button"]')
+  .click()
+},
+
+inputUsername(username){
+  cy.get('[data-test="username"]')
+    .type(username)
+    .should('have.value', username)
+},
+
+inputPassword(password){
+  cy.get('[data-test="password"]')
+    .type(password)
+    .should('have.value', password)
+},
+
+}
